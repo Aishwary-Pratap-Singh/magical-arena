a08dc8093df18ec1c8a439e2a36acd7ac7aa2c6b
Aishwary Pratap Singh <aishwarypsingh@gmail.com>
2024-06-30 11:37:27 +0530

    added start fight logic to game class

--- src/Game.js
+++ src/Game.js
@@ -25,6 +25,32 @@ class Game {
 
         Logger.info(`Game created: Player A (Health=${playerA.health}, Strength=${playerA.strength}, Attack=${playerA.attack}), Player B (Health=${playerB.health}, Strength=${playerB.strength}, Attack=${playerB.attack})`);
     }
+
+    /**
+     * Starts the fight between the two players.
+     */
+    startFight() {
+        Logger.info('Fight started');
+        let attacker = (this.playerA.health < this.playerB.health) ? this.playerA : this.playerB;
+        let defender = (attacker === this.playerA) ? this.playerB : this.playerA;
+
+        while (this.playerA.isAlive() && this.playerB.isAlive()) {
+            Logger.info(`Attacker: Player ${attacker === this.playerA ? 'A' : 'B'}, Defender: Player ${defender === this.playerA ? 'A' : 'B'}`);
+            attacker.attackOpponent(defender);
+            if (!defender.isAlive()) break;
+
+            [attacker, defender] = [defender, attacker];
+        }
+
+        if (this.playerA.isAlive()) {
+            Logger.info("Player A wins!");
+            console.log("Player A wins!");
+        } else {
+            Logger.info("Player B wins!");
+            console.log("Player B wins!");
+        }
+        Logger.info('Fight ended');
+    }
 }
 
 module.exports = Game;
